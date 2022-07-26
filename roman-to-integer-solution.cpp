252845a14496c4cc0b177e5315f2304f741aeac6
zlauzon <9244781+zlauzon@users.noreply.github.com>
2022-07-26 11:54:26 -0600

    Create roman-to-integer-solution.cpp

---
+++ roman-to-integer-solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  int romanToInt(string s) {
+    int num = 0;
+    char lastChar = 0;
+    for (char letter : s) {
+      switch (letter) {
+        case 'M': num += 500;
+        case 'D': num += 500 - lastChar == 'C' ? 200 : 0; break;
+        case 'C': num += 50;
+        case 'L': num += 50 - lastChar == 'X' ? 20 : 0; break;
+        case 'X': num += 5;
+        case 'V': num += 5 - lastChar == 'I' ? 2 : 0; break;
+        case 'I': num += 1;
+      }
+      lastChar = letter;
+    }
+    return num;
+  }
+};
